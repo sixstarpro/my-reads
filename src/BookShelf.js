4967d68d738259c123838154870b9b882c8a48bf
sixstarpro <sixstarpro@saads-MacBook-Pro-2.local>
2018-01-09 17:53:09 -0500

    my intial files upload

---
+++ src/BookShelf.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+class BookShelf extends Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        onUpdateShelf: PropTypes.func.isRequired
+    }
+
+    // state = {
+    //     query: ''
+    // }
+
+    // updateQuery = (query) => {
+    //     this.setState({ query: query.trim() })
+    // }
+
+    // clearQuery = () => {
+    //     this.setState({ query: '' })
+    // }
+
+    // logShelf = (e) => {
+    //   const values = e
+    //   console.log(values)
+    // }
+    render() {
+        const { books, onUpdateShelf } = this.props
+        // const { query } = this.state
+
+        // let showingContacts
+        // if (query) {
+        //     const match = new RegExp(escapeRegExp(query), 'i')
+        //     showingContacts = contacts.filter((contact) => match.test(contact.name))
+        // } else {
+        //     showingContacts = contacts
+        // }
+
+        // showingContacts.sort(sortBy('name'))
+        function getShelf(shelf) {
+          return books.filter(bookShelf => bookShelf.shelf === shelf)
+        }
+        return (
+
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {getShelf('currentlyReading').map((books) => (
+                        <li key={books.id}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${books.imageLinks && books.imageLinks.thumbnail ? books.imageLinks.thumbnail : 'http://via.placeholder.com/128x193/fffef1/60ac5d?text=No+Cover'})` }}></div>
+                              <div className="book-shelf-changer">
+                                <select
+                                  onChange={(e) => onUpdateShelf(books, e.target.value)}
+                                  value={books.shelf}
+                                >
+                                  <option value="none" disabled>Move to...</option>
+                                  <option value="currentlyReading">Currently Reading</option>
+                                  <option value="wantToRead">Want to Read</option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{books.title}</div>
+                            <div className="book-authors">{books.authors ? books.authors.map((author, index) => (
+                                index === 0 ? author : ', ' + author 
+                            )) : ''}
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {getShelf('wantToRead').map((books) => (
+                        <li key={books.id}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${books.imageLinks && books.imageLinks.thumbnail ? books.imageLinks.thumbnail : 'http://via.placeholder.com/128x193/fffef1/60ac5d?text=No+Cover'})` }}></div>
+                              <div className="book-shelf-changer">
+                              <select
+                                  onChange={(e) => onUpdateShelf(books, e.target.value)}
+                                  value={books.shelf}
+                                >
+                                  <option value="none" disabled>Move to...</option>
+                                  <option value="currentlyReading">Currently Reading</option>
+                                  <option value="wantToRead">Want to Read</option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{books.title}</div>
+                            <div className="book-authors">{books.authors ? books.authors.map((author, index) => (
+                                index === 0 ? author : ', ' + author 
+                            )) : ''}
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {getShelf('read').map((books) => (
+                        <li key={books.id}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${books.imageLinks && books.imageLinks.thumbnail ? books.imageLinks.thumbnail : 'http://via.placeholder.com/128x193/fffef1/60ac5d?text=No+Cover'})` }}></div>
+                              <div className="book-shelf-changer">
+                                <select
+                                  onChange={(e) => onUpdateShelf(books, e.target.value)}
+                                  value={books.shelf}
+                                >
+                                  <option value="none" disabled>Move to...</option>
+                                  <option value="currentlyReading">Currently Reading</option>
+                                  <option value="wantToRead">Want to Read</option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{books.title}</div>
+                            <div className="book-authors">{books.authors ? books.authors.map((author, index) => (
+                                index === 0 ? author : ', ' + author 
+                            )) : ''}
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to='/search'>Add a book</Link>
+            </div>
+          </div>
+
+        )
+      }
+    }
+
+export default BookShelf
---
+++ src/SearchBooks.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+// import escapeRegExp from 'escape-string-regexp'
+// import serializeForm from 'form-serialize'
+
+class SearchBooks extends Component {
+    static propTypes = {
+        searchData: PropTypes.array.isRequired,
+        onSearch: PropTypes.func.isRequired,
+        getShelf: PropTypes.func,
+        onUpdateShelf: PropTypes.func.isRequired
+    }
+
+    // state = {
+    //     query: ''
+    // }
+
+    // updateQuery = (query) => {
+    //     this.setState({ query: query })
+    //     this.props.onSearch(query)
+    // }
+    // callSearch = (term) => {
+    //     let searchBooks
+    //     searchBooks = this.onSearch(term)
+    // }
+    getCurrentShelf = ( id ) => {
+        this.props.getShelf(id)
+    }
+    render() {
+        const { searchData, onSearch, onUpdateShelf, getShelf } = this.props
+        // const { query } = this.state
+        
+        // let searchBooks
+        // if (query) {
+        //     searchBooks = searchData.map((data) => data.id )
+        //     console.log('searchBooks: ' + searchBooks)
+        // }
+        // getShelf('EmOGpdzhITIC')
+        // searchBooks = onSearch('basketball')
+        // this.callSearch('basketball')
+        
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to='/'>Close</Link>
+                    <div className="search-books-input-wrapper">
+                        {/*
+                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                        You can find these search terms here:
+                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                        you don't find a specific author or title. Every search is limited by search terms.
+                        */}
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            // value={query}
+                            onChange={(e) => onSearch(e.target.value)}
+                        />
+
+                    </div>
+                </div>
+
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {searchData.map((books) => (
+                            <li key={books.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${books.imageLinks && books.imageLinks.thumbnail ? books.imageLinks.thumbnail : 'http://via.placeholder.com/128x193/fffef1/60ac5d?text=No+Cover'})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select
+                                                onChange={(e) => onUpdateShelf(books, e.target.value)}
+                                                value={this.getCurrentShelf(books.id)}
+                                            >
+                                                <option value="" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{books.title}</div>
+                                    <div className="book-authors">{books.authors? books.authors.map((author, index) => (
+                                        index === 0 ? author : ', ' + author 
+                                    )) : ''}
+                                    </div>
+                                </div>
+                                <button onClick={() => getShelf(books.id)}>Click</button>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBooks
